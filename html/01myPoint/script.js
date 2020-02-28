14ba54d7106fff1e07d2d49a96b6bac15ca9f19b
unknown <thomas.berrens@me.com>
2020-02-28 14:55:52 +0100

    Feature to instantiate and draw points

--- html/01myPoint/script.js
+++ html/01myPoint/script.js
@@ -8,3 +8,11 @@ canvas.width = width;
 canvas.height = height;
 
 // begin hier met jouw code voor deze opdracht
+let myPoint = new Point(500,200, 50, "blue");
+myPoint.draw(context);
+
+let myPoint2 = new Point(600,300, 25, "green");
+myPoint2.draw(context);
+
+let myPoint3 = new Point(700,400, 25, "black");
+myPoint3.draw(context);
